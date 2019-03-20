62a06a60e3c7295fbd386401a06ce1a1efa7b5b7
Jocelyn Huang <ilikeoctopus@gmail.com>
2019-03-20 18:09:02 -0400

    Script for stripping tracks from the original data

--- process_data.py
+++ process_data.py
@@ -1,19 +1,30 @@
 import numpy as np
 import time
 
-def read_large():
-    # shape = [n, #bars, #timesteps/bar, #pitches, #tracks]
+large_file = '../musegan/data/train_x_lpd_5_phr.npz'
+small_file = 'small_data.npy'
 
-    filepath = '../musegan/data/train_x_lpd_5_phr.npz'
-    with np.load(filepath) as loaded:
+def process_original_data(save_small=False):
+    # shape = [n, #bars, #timesteps/bar, #pitches, #tracks]
+    # Want:   [n, #bars * #tracks, #timesteps/bar, #pitches]
+
+    with np.load(large_file) as loaded:
         shape = loaded['shape']
         data = np.zeros(shape=shape).astype(bool)
+
+        # Create matrix
         stime = time.time()
         data[[x for x in loaded['nonzero']]] = True
+        print("Time taken to load: %s" % (time.time()-stime))
 
-        print("Time taken: %s" % (time.time()-stime))
+        # Cut to two tracks
+        stime = time.time()
+        data = data[:, :, :, :, 3:]
 
-        np.save('small_tracks.npy', data[:100])
+        np.save('full_data.npy', data)
+
+        if save_small:
+            np.save(small_file, data[:100])
 
 def read_small():
     filepath = 'small_tracks.npy'
@@ -22,4 +33,4 @@ def read_small():
         
 
 if __name__ == '__main__':
-    read_small()
+    process_original_data(save_small=True)
